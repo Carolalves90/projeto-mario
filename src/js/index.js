693388961dae311f607cd7fd080a3d2ef5f57a28
Carolalves90 <carolalves.tech@gmail.com>
2023-01-22 18:09:09 -0300

    adiciona js

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+/*  1 - quando clicar no botao de ver trailer
+abria modal com o video do trailer.
+        passo 1 - pegar o elemento que representa o botao de veja o trailer;
+        passo 2 - pegar elemento de fechar modal usando o js
+        passo 3 - identicar quando o usario clicar no botao;
+        passo 4 - elemento modal no js;
+        passo 5 - abril a modal na tela.
+
+    2 - quando o usuario clicar no X devemos fechar a modal;
+        passo 1 - pegar elemento de fechar modal já declarada;
+        passo 2 - identificar quando o usuario clicar no X;
+        passo 3 - fechar modal
+*/
+
+const botaoTrailer = document.querySelector(".botao-trailer")
+const botaoFecharModal = document.querySelector(".fechar-modal")
+const video = document.getElementById("video")
+const modal = document.querySelector(".modal")
+const linkDoVideo = video.src
+
+botaoTrailer.addEventListener("click", () => {
+    modal.classList.add("aberto") 
+    video.setAttribute("src", linkDoVideo)
+})
+
+botaoFecharModal.addEventListener("click", () => {
+    modal.classList.remove("aberto")
+    video.setAttribute("src", " ")
+})
